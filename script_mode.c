b498c144c717ca0e9deeef3f3282a32db2d2d336
Atiwat-R <75544677+Atiwat-R@users.noreply.github.com>
2021-05-17 01:19:03 +0700

    Update script_mode.c

--- script_mode.c
+++ script_mode.c
@@ -18,28 +18,5 @@ void process_script_mode(char *argv[]) {
 }
 
 
-// int main ( int argc, char *argv[] ) // argc is number of files, including this one.
-// { 
-//     if ( argc != 2 ) /* argc should be 2 for ./a.out and filename.txt */ 
-//     { 
-//         printf( "usage: %s is provided,filename is not provided", argv[0] ); 
-//     } 
-//     else  
-//     { 
-//         FILE *file = fopen( argv[1], "r" ); 
-//         if ( file == 0 ) 
-//         { 
-//             printf( "Could not open file\n" ); 
-//         } 
-//         else  
-//         { 
-//             int x; 
-//             while  ( ( x = fgetc( file ) ) != EOF ) 
-//             { 
-//                 printf( "%c", x ); 
-//             } 
-//             fclose( file ); 
-//         } 
-//     } 
-// } 
+
 
