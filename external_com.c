c9258cccfdf362484857d0604541a6dc631092bd
Atiwat-R <75544677+Atiwat-R@users.noreply.github.com>
2021-05-27 03:32:00 +0700

    Add files via upload

--- external_com.c
+++ external_com.c
@@ -34,24 +34,50 @@ int is_external_com(char input[100]) {
 
 // Execute external command
 void execute_external_com(char input[100]) {
-    input[strlen(input)-1] = '\0';
-    char extcom_path[100] = "/bin/"; // Path to external commands on device
-    strcat(extcom_path, input); // Concat to extcom_path
-    char *all_words = strtok(extcom_path, " "); // Split by spaces
+
+    input[strlen(input)-1] = '\0'; // Eliminate \n and leave placeholder for NULL
+    char extcom_path[100] = "/bin/ls"; // Path to external commands on device
+    // strcat(input, " NULLPH"); 
+
+    char *all_words = strtok(input, " "); // Split by spaces
+
+    char *cmd[100];
+    int cntr = 0;
+    while (all_words != NULL) {
+        cmd[cntr] = all_words;
+        all_words = strtok(NULL, " ");
+        cntr++;
+    }
+    cmd[cntr] = NULL;
+
+    // size_t n = sizeof(all_words) / sizeof(all_words[0]); // Get array size
+    // all_words[n-1] = NULL; // Use n to set last index to NULL
+
+
+    // // Concat to extcom_path
+    // char comm[100];
+    // strcpy(comm, all_words);
+    // strcat(extcom_path, all_words); 
 
 
     int status;
-    char *args[100];
+    // char *args[100];
 
-    args[0] = all_words;        // first arg is the full path to the executable
-    all_words = strtok(NULL, " ");
+    // args[0] = all_words;        // first arg is the full path to the executable
+    // all_words = strtok(NULL, " ");
 
-    args[1] = all_words; // Rest of the argument, if any
-    args[2] = NULL; // list of args must be NULL terminated
+    // args[1] = all_words; // Rest of the argument, if any
+    // args[2] = NULL; // list of args must be NULL terminated
+
+    // while (all_words != NULL) {
+    //     printf("->%s\n", all_words);
+    //     all_words = strtok(NULL, " ");
+    // }
+      
 
     // Fork and Execute external files
     if ( fork() == 0 )
-        execv( args[0], args ); // child: call execv with the path and the args
+        execv( extcom_path, cmd ); // child: call execv with the path and the args
     else
         wait( &status );        // parent: wait for the child (not really neces>
 
